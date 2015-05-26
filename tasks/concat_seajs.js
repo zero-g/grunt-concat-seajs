891ba3975fd18d80ef7e80bceabc534591ffb8b2
zero-g <18959260204@163.com>
2015-05-26 10:13:57 +0800

    support reg files and more view file

--- tasks/concat_seajs.js
+++ tasks/concat_seajs.js
@@ -21,24 +21,87 @@ module.exports = function(grunt) {
     var options = this.options({
       map_name: 'fetch.js'
     });
-    if (!options.seajs_src || !options.view_src) {
-      grunt.log.warn('seajs_src or view_src has not configured');
+    if (!options.seajs_src) {
+      grunt.log.warn('seajs_src  has not configured');
       return;
     }
     var mapFileSrc = options.seajs_src + options.map_name;
-    var viewSrc = options.view_src;
+    var baseDir=options.baseDir;
     var mapFileName = options.map_name;
-
-    createMapFile(mapFileSrc); //general
+    createMapFile(mapFileSrc,baseDir); //general
     grunt.log.writeln('build map file for seajs concat project', mapFileSrc);
     if (grunt.filerev && grunt.filerev.summary) {
       mapFileName = md5File(mapFileSrc);
       grunt.log.writeln('map file for md5：', mapFileName);
     }
 
-    appendMapFileToView(viewSrc, mapFileName); //append map file to the view file where seajs is
-    grunt.log.writeln('append fetch file to：', viewSrc);
+      this.files.forEach(function(filePair) {
+          filePair.src.forEach(function(file) {
+              console.log(file);
+               //append map file to the view file where seajs is
+              appendMapFileToView(file, mapFileName);
+              grunt.log.writeln('append fetch file to：', viewSrc);
+          });
+      });
+
   });
+
+    function once(fn) {
+        var called = false;
+
+        if (typeof fn !== 'function') {
+            throw new TypeError;
+        }
+
+        return function () {
+            if (called) {
+                throw new Error('Callback already called.');
+            }
+            called = true;
+            fn.apply(this, arguments);
+        }
+    }
+
+    function eachAsync(arr, next, cb) {
+        var failed = false;
+        var count = 0;
+
+        cb = cb || function () {};
+
+        if (!Array.isArray(arr)) {
+            throw new TypeError('First argument must be an array');
+        }
+
+        if (typeof next !== 'function') {
+            throw new TypeError('Second argument must be a function');
+        }
+
+        var len = arr.length;
+
+        if (!len) {
+            return cb();
+        }
+
+        function callback(err) {
+            if (failed) {
+                return;
+            }
+
+            if (err !== undefined && err !== null) {
+                failed = true;
+                return cb(err);
+            }
+
+            if (++count === len) {
+                return cb();
+            }
+        }
+
+        for (var i = 0; i < len; i++) {
+            next(arr[i], i, once(callback));
+        }
+    }
+
 
   function md5File(file) {
 
@@ -66,14 +129,14 @@ module.exports = function(grunt) {
     grunt.file.write(viewSrc, code);
   }
 
-  function createMapFile(mapFileSrc) {
+  function createMapFile(mapFileSrc,baseDir) {
     var FETCH_TEMPLATE = 'seajs.on("fetch", function(data) {\n' + '\tvar cfm = concFilesMath;\n' + '\tfor(var beConfFile in cfm) {\n' + '\t\tdata.requestUri = data.uri.replace(beConfFile, cfm[beConfFile]);\n' + '\t\tif(data.uri !== data.requestUri) { break;}\n' + '\t};\n' + '});';
 
     //获取任务配置
     var filesMatch = getConcatFilesMatch();
 
     //生成map_name文件
-    var code = createMapCode(filesMatch);
+    var code = createMapCode(resovePath(filesMatch,baseDir));
     grunt.file.write(mapFileSrc, code);
     return code;
 
@@ -97,6 +160,8 @@ module.exports = function(grunt) {
       }
 
       var summary = grunt.filerev ? grunt.filerev.summary: null;
+
+       //console.log('summary:'+JSON.stringify(grunt.filerev));
       if (!summary) {
         grunt.log.write('没有进行md5');
       } else if (!concFiles) {
@@ -120,10 +185,21 @@ module.exports = function(grunt) {
 
       return concFilesMath;
     }
+    function resovePath(concFilesMath,baseDir){
+        var resultMap={};
+        for (var src in concFilesMath) {
+            // console.log('key:'+src+ ' '+' value:'+concFilesMath[src]);
+            resultMap[src.substr(baseDir.length)]=concFilesMath[src].substr(baseDir.length);
+
+        }
+        return resultMap;
+    }
     function createMapCode(concFilesMath) {
+
       var code = FETCH_TEMPLATE.replace('concFilesMath', JSON.stringify(concFilesMath));
       return code;
     }
+
   }
 
 };
