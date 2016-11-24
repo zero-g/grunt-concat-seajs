c117f68292e51e76045621c5125e844547fbe5a4
shaokaiming <shaokaiming@weidian.com>
2016-11-24 13:11:05 +0800

    regexp bug fixed

--- tasks/concat_seajs.js
+++ tasks/concat_seajs.js
@@ -199,7 +199,7 @@ module.exports = function(grunt) {
         var cdnBase = options.cdnBase;
         var baseDir = options.baseDir;
         var source = codeMap['default']['code'];
-        var seajsReg = /<script.*?(sea.*?js).*?<\/script>/i;
+        var seajsReg = /<script[^<]*?src.*?(sea\.*?js).*?<\/script>/i;// /<script.*?(sea.*?js).*?<\/script>/i;
         var m = code.match(seajsReg);
         if (!m) {
             return;
@@ -240,7 +240,7 @@ module.exports = function(grunt) {
             cdnBase = options.cdnBase,
             baseDir = options.baseDir;
 
-        var seajsReg = /<script.*?(sea.*?js).*?<\/script>/i;
+        var seajsReg = /<script[^<]*?src.*?(sea\.*?js).*?<\/script>/i;
         var m = code.match(seajsReg);
         if (!m) {
             return;
@@ -479,7 +479,6 @@ module.exports = function(grunt) {
          * @returns {string}
          */
         function createMapCode(concFilesMath) {
-            //var FETCH_TEMPLATE = 'seajs.on("fetch", function(data) {\n' + '\tvar cfm = concFilesMath;\n' + '\tfor(var beConfFile in cfm) {\n' + '\t\tdata.requestUri = data.uri.replace(beConfFile, cfm[beConfFile]);\n' + '\t\tif(data.uri !== data.requestUri) { break;}\n' + '\t};\n' + '});';
             var FETCH_TEMPLATE = '\nseajs.on("fetch", function(data) {\n' + '\tvar cfm = concFilesMath;\n' + '\tvar mod=data.uri.replace(seajs.data.base,"");\n' + '\tdata.requestUri = data.uri;\n' + '\tif(cfm[mod]){\n' + '\t\tdata.requestUri = data.uri.replace(mod, cfm[mod]);\n' + '\t}\n'   + '\t});\n';
             var code = FETCH_TEMPLATE.replace('concFilesMath', JSON.stringify(concFilesMath));
             return code;
